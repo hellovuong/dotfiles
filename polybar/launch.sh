adfc6942cf883bb361c68c5c2540fb9f1f34b617
Long Vuong <vuong@wheel.me>
2024-04-11 17:45:06 +0200

    polybar: show bar in multi monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,10 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-MONITORS=$(xrandr --query | grep " connected" | cut -d" " -f1)
-
-MONITORS=$MONITORS polybar top &
-MONITOR=$MONITORS polybar bottom;
+# Launch bar
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload top &
+done
+fi
 
 echo "Bars launched..."
