3bd7d63207511fe92edb220e40aa3d4b0460015e
Long Vuong <vuong@wheel.me>
2024-09-13 13:01:57 +0200

    polybar Fix wrong font name and bug in launch script

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,12 +4,11 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $(id -u) -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload top &
 done
-fi
 
 echo "Bars launched..."
